b44fa0b3b9f93c73f490fe6392201d93e6c68372
Gideonchandar <114631460+Gideonchandar@users.noreply.github.com>
2024-04-14 17:32:05 +0700

    Add files via upload

---
+++ ContohArrayList.java
@@ -0,0 +1,25 @@
+package Sesi7;
+
+import java.util.ArrayList;
+
+public class ContohArrayList {
+
+    public static void main(String[] args) {
+ 
+        ArrayList<String> nama = new ArrayList<>();
+
+        nama.add("Gideon");
+        nama.add("Chandra");
+        nama.add("Dwianto");
+
+        System.out.println("Isi ArrayList: " + nama);
+
+        nama.remove("Dwianto");
+
+        boolean adaSatria = nama.contains("Gideon");
+
+        System.out.println("Apakah ada Gideon? " + adaGideo;)
+
+        System.out.println("Isi ArrayList setelah dihapus: " + nama);
+    }
+}
